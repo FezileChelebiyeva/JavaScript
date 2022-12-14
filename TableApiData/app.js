0ab1230ab03bb86ef2e26fb5a7074eb6bf9eb1be
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2022-12-15 02:25:00 +0400

    delete and put  api, table

--- TableApiData/app.js
+++ TableApiData/app.js
@@ -1,24 +1,26 @@
 const BASE_URL = "https://northwind.vercel.app/api/suppliers";
 let tbody = document.querySelector("tbody");
+let compName = document.querySelector("#companyName");
+let contName = document.querySelector("#contactName");
+let contTitle = document.querySelector("#contactTitle");
+let submitBtn = document.querySelector(".submitBtn");
 
 function usersData() {
   fetch(`${BASE_URL}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-
       data.forEach((element) => {
-        console.log(element);
         let trElement = document.createElement("tr");
         trElement.innerHTML = `
                       <td>${element.id}</td>
                       <td>${element.companyName}</td>
                       <td>${element.contactName}</td>
                       <td>${element.contactTitle}</td>
-                      <td><button class="editBtn">Edit</button></td>
+                      <td><button id=${element.id} class="editBtn">Edit</button></td>
                       <td><button id=${element.id} class="deleteBtn">Delete</button></td>
                       `;
         tbody.appendChild(trElement);
+        submitBtn.setAttribute("id", `${element.id}`);
       });
       let deleteBtn = document.querySelectorAll(".deleteBtn");
       deleteBtn.forEach((element) => {
@@ -29,6 +31,43 @@ function usersData() {
             method: "DELETE",
           });
         });
+      });
+
+      let editedElementId;
+
+      let editBtn = document.querySelectorAll(".editBtn");
+      editBtn.forEach((element) => {
+        element.addEventListener("click", function (e) {
+          editedElementId = this.id;
+          e.preventDefault();
+          fetch(`https://northwind.vercel.app/api/suppliers/${this.id}`)
+            .then((response) => response.json())
+            .then((data) => {
+              compName.value = data.companyName;
+              contName.value = data.contactName;
+              contTitle.value = data.contactTitle;
+            });
+        });
+      });
+
+      submitBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let putObj = {
+          companyName: compName.value,
+          contactName: contName.value,
+          contactTitle: contTitle.value,
+        };
+        console.log(putObj);
+
+        fetch(`https://northwind.vercel.app/api/suppliers/${editedElementId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(putObj),
+        });
+        usersData();
       });
     });
 }
